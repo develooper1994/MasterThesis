55317a43cd77b2024eaa802373dbf8d8c6c15c1f
selcuk <selcukcaglar08@gmail.com>
2020-01-09 13:59:11 +0300

    Mster Thesis experimentations

---
+++ utils/config.py
@@ -0,0 +1,10 @@
+# Training
+EPOCHS = 10  # 180
+BATCH_SIZE = 64  # 64  # 10
+
+SAMPLE_EVERY = 1  # Generate audio samples every 1 epoch.
+SAMPLE_NUM = 10  # Generate 10 samples every sample generation.
+
+# Data
+DATASET_NAME = "/home/selcuk/.pytorch/sc09/"  # 'sc09/'
+OUTPUT_PATH = "output/"
