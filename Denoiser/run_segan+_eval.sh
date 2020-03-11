37f9c7d8dfff26c31aab3fe11a4b28c77e39bfbe
Mustafa Selçuk Çağlar <selcukcaglar08@gmail.com>
2020-03-11 11:14:41 +0300

    bash scripts fixed and evaluation script add you should set train and test datasets in your enviroment

---
+++ Denoiser/run_segan+_eval.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+#python3 -u train.py --save_path ckpt_segan+ \
+#                     --clean_valset $clean_test_wav\
+#                     --noisy_valset $noisy_test_wav \
+
+# --g_pretrained_ckpt $pretrained is experimental to speed up training
+python3 -u train.py --save_path ckpt_segan_sinc+ \
+        --clean_valset $clean_test_wav\
+        --noisy_valset $noisy_test_wav \
+        --logfile logdir
