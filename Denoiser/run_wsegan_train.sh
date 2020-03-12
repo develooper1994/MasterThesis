cb174f532983d8deea618232494f5878e01ec33a
Mustafa Selçuk Çağlar <selcukcaglar08@gmail.com>
2020-03-12 23:04:21 +0300

    bash scripts fixed and evaluation script add you should set train and test datasets in your enviroment clean.py opts.test_files bug fixed

--- Denoiser/run_wsegan_train.sh
+++ Denoiser/run_wsegan_train.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 
-#python -u train.py --save_path ckpt_wsegan_misalign \
-#	--clean_trainset data_veu4/silent/clean_trainset_M4 \
-#	--noisy_trainset data_veu4/silent/whisper_trainset_M4 \
-#	--cache_dir data_silent_cache --no_train_gen --batch_size 150  \
-#	--wsegan --gnorm_type snorm --dnorm_type snorm --opt adam \
-#	--data_stride 0.05 --misalign_pair
+#python3 -u train.py --save_path ckpt_wsegan_misalign \
+#        --clean_trainset data_veu4/silent/clean_trainset_M4 \
+#        --noisy_trainset data_veu4/silent/whisper_trainset_M4 \
+#        --cache_dir data_silent_cache --no_train_gen --batch_size 150  \
+#        --wsegan --gnorm_type snorm --dnorm_type snorm --opt adam \
+#        --data_stride 0.05 --misalign_pair
 
 # --batch_size 150
-python -u train.py --save_path ckpt_wsegan_misalign \
+python3 -u train.py --save_path ckpt_wsegan_misalign \
         --clean_trainset $clean_train56spk_wav \
         --noisy_trainset $noisy_train56spk_wav \
         --clean_valset $clean_test_wav\
